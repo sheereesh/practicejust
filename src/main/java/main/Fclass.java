38540086790d7ad78a9433423d02ad84d3d755e9
sheereesh <alekh99@gmail.com>
2022-02-28 21:47:35 +0530

    added fck

---
+++ src/main/java/main/Fclass.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Fclass {
+
+	public static void main(String[] args) {
+	System.out.println("im the oneee");
+System.out.println("im no way relateed");
+
+System.out.println("im the oneeeeit");
+	}
+
+}
